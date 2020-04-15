e8cdb624e0b3fcc2783300d7c3c668bc0892a390
Bartek <WonderfulLifeVision@gmail.com>
2020-04-15 22:54:37 +0200

    updated start-date data

--- src/app/countries/countries.ts
+++ src/app/countries/countries.ts
@@ -2,22 +2,44 @@ import { Country } from './country-template';
 
 export const COUNTRIES: Country[] = [
   { name: 'Italy', startDate: '2020-01-31T15:30:00+02:00', endDate: '2020-05-04T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'France', startDate: '2020-01-24T14:28:00+02:00', endDate: '2020-05-11T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Poland', startDate: '2020-03-04T08:00:00+02:00', endDate: '2020-05-04T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'China', startDate: '2019-12-17T15:30:00+02:00', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'USA', startDate: '', endDate: '2020-06-16T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Turkey', startDate: '', endDate: '2020-04-30T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'India', startDate: '', endDate: '2020-05-03T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Japan', startDate: '', endDate: '2020-05-06T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Norway', startDate: '', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Denmark', startDate: '2020-02-27T01:00:00+02:00', endDate: '2020-05-10T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Germany', startDate: '', endDate: '2020-04-20T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Austria', startDate: '', endDate: '2020-05-01T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'UK', startDate: '', endDate: '2020-09-01T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
 
-  { name: 'Tasmania', startDate: '', endDate: '2020-04-28T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'South Australia', startDate: '', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Western Australia', startDate: '', endDate: '2020-04-28T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
-  { name: 'Queesland', startDate: '', endDate: '2020-04-20T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' }
+  { name: 'France', startDate: '2020-01-24T14:28:00+02:00', endDate: '2020-05-11T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Poland', startDate: '2020-03-04T08:00:00+02:00', endDate: '2020-04-26T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'China', startDate: '2019-12-17T15:30:00+02:00', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'USA', startDate: '2020-01-20T01:00:00+02:00', endDate: '2020-06-16T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Turkey', startDate: '2020-03-11T01:00:00+02:00', endDate: '2020-04-30T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'India', startDate: '2020-01-30T01:00:00+02:00', endDate: '2020-05-03T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Japan', startDate: '2020-01-10T01:00:00+02:00', endDate: '2020-05-06T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Switzerland', startDate: '2020-02-25T01:00:00+02:00', endDate: '', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Norway', startDate: '2020-02-26T01:00:00+02:00', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Denmark', startDate: '2020-02-27T01:00:00+02:00', endDate: '2020-05-10T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Germany', startDate: '2020-01-27T01:00:00+02:00', endDate: '2020-04-20T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Austria', startDate: '2020-02-25T01:00:00+02:00', endDate: '2020-05-01T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'UK', startDate: '2020-01-29T01:00:00+02:00', endDate: '2020-09-01T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+
+  { name: 'New Zealand', startDate: '2020-02-28T01:00:00+02:00', endDate: '2020-04-29T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Tasmania', startDate: '2020-03-01T01:00:00+02:00', endDate: '2020-04-28T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'South Australia', startDate: '2020-02-01T01:00:00+02:00', endDate: '2020-04-27T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Western Australia', startDate: '2020-02-29T01:00:00+02:00', endDate: '2020-04-28T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Queesland', startDate: '2020-01-25T01:00:00+02:00', endDate: '2020-04-20T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' },
+
+  { name: 'Sri Lanka', startDate: '2020-01-27T01:00:00+02:00', endDate: '2020-05-04T01:00:00+02:00', leftPast: { days: '', hours: '', minutes: '', seconds: '' }, leftFuture: { days: '', hours: '', minutes: '', seconds: '' }, startLink: '', endLink: '' }
 
 ];
