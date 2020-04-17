270850dcf0495148058ea558ec33c13253ecfb23
Bartek <WonderfulLifeVision@gmail.com>
2020-04-17 20:48:35 +0200

    dynamic references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { MatCardModule } from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +16,7 @@ import { LastLineComponent } from './footer/lastLine/lastLine.component';
 import { ReferencesComponent } from './footer/references/references.component';
 import { TimerListComponent } from './body/timer-template/timer-template.component';
 import { PandDescComponent } from "./body/pandemic-desc/pandemic-desc.component";
-import { DatePipe } from '@angular/common';
+import { DatePipe, CommonModule } from '@angular/common';
 import { FilterPipe } from './filter.pipe';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -38,7 +39,8 @@ import { AppRoutingModule } from './app-routing.module';
     AngularSvgIconModule.forRoot(),
     Ng2SearchPipeModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
   providers: [DatePipe, HeaderComponent],
   bootstrap: [AppComponent]
--- src/app/footer/references/references.component.ts
+++ src/app/footer/references/references.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Country } from '../../countries/country-template';
+import { CountryService } from 'src/app/country.service';
+
 @Component({
   selector: 'app-footer-references',
   templateUrl: './references.component.html',
   styleUrls: ['./references.component.css']
 })
 
-export class ReferencesComponent {}
+export class ReferencesComponent {
+
+  countries: Country[];
+
+  constructor( countryService: CountryService ) {
+    this.countries = countryService.getCountries();
+  }
+
+}
