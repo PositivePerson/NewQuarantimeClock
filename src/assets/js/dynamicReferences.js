c9ddbb0297d18e58b51d72111342fe53266b31c5
Bartek <WonderfulLifeVision@gmail.com>
2020-04-18 11:44:12 +0200

    dynamic height for dynamic references

---
+++ src/assets/js/dynamicReferences.js
@@ -0,0 +1,11 @@
+
+function changeHeight(countriesAmount) {
+  var height = 4.5 + countriesAmount*2.9;
+
+  console.log("height is: " + height);
+  document.head
+  .insertAdjacentHTML('beforeend',
+  '<style>#references:hover{height:' + height + 'em;}</style>');
+  // return (2.5 + countriesAmount*2.9);
+  console.log("adding inline style done");
+}
